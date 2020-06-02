013a60ac2d5d273c65b4ee93d13d2074bb5d0be1
yegor <yegoryanushkevich@gmail.com>
2020-06-02 07:21:07 +0200

    initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,146 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, paragraph4, resume } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <Container>
+        <Title title="About Me" />
+        <Row className="about-wrapper">
+          <Col md={6} sm={12}>
+            <Row>
+              <Fade bottom duration={1000} delay={600} distance="30px">
+                <div
+                  className="about-wrapper__image"
+                  style={{
+                    alignItems: 'baseline',
+                    justifyContent: 'end',
+                    boxShadow: '0px 4px 20px rgba(0,0,0,0.25)',
+                  }}
+                >
+                  <AboutImg alt="profile picture" filename={img} />
+                </div>
+              </Fade>
+            </Row>
+            <Row>
+              <Fade bottom duration={1000} delay={600} distance="30px">
+                <div className="about-wrapper__info">
+                  <br />
+                  <br />
+                  <br />
+                  <p className="about-wrapper__info-text">
+                    <b>Country: Poland</b>
+                  </p>
+                  <br />
+                  <p className="about-wrapper__info-text">
+                    <b>Hourly rate: $40/hr</b>
+                  </p>
+                  <p className="about-wrapper__info-text">
+                    <b>Available: More than 40hrs/week</b>
+                  </p>
+                </div>
+              </Fade>
+            </Row>
+          </Col>
+          <Col md={6} sm={12}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="about-wrapper__info">
+                <p className="about-wrapper__info-text">
+                  {paragraphOne ||
+                    'A seasoned full stack developer with a flair for creating elegant solutions in the least amount of time.'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphTwo ||
+                    'Specialized in web application development such as E-Commerce, Enterprise Application, SaaS Platform, SPA, Health Care System, CRM and Learning Management System.'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphThree || 'Love working with great team with Agile/Scrum methodology.'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  <b>{paragraph4 || ''}</b>
+                </p>
+                <p className="about-wrapper__info-text">
+                  &nbsp;&nbsp;&nbsp;• ES6 + JavaScript, TypeScript, HTML5, CSS3, SCSS, jQuery
+                  <br />
+                  &nbsp;&nbsp;&nbsp;• React.js, Redux-Thunk, Redux-Saga, React Hooks, Context API
+                  <br />
+                  &nbsp;&nbsp;&nbsp;• NextJS, GatsbyJS
+                  <br />
+                  &nbsp;&nbsp;&nbsp;• Angular 2-9, NgRx, RxJS, Angular Material
+                  <br />
+                  &nbsp;&nbsp;&nbsp;• REST, GraphQL, Apollo
+                  <br />
+                  &nbsp;&nbsp;&nbsp;• Vue.js,VueX, Nuxt.js <br />
+                  &nbsp;&nbsp;&nbsp;• Ant Design, Semantic, UI Kit, Styled Component, Material,
+                  Bootstrap
+                </p>
+                <p className="about-wrapper__info-text">
+                  <b>- Back-end</b>
+                </p>
+                <p className="about-wrapper__info-text">
+                  &nbsp;&nbsp;&nbsp;• NodeJS, PHP, Python, Ruby On Rails
+                  <br />
+                </p>
+                <p className="about-wrapper__info-text">
+                  I have strong debugging and troubleshooting skills that allow me to quickly
+                  analyze problems and come to a modern solution that extends the life of the
+                  application without rewriting everything from scratch.
+                </p>
+                <p className="about-wrapper__info-text">
+                  My gift is to solve complex problems through code while maintaining the
+                  perspective of an end user—keeping simplicity first and foremost in the product
+                  design—then ensuring that the design and functionality of the product work in
+                  perfect harmony.
+                </p>
+                <p className="about-wrapper__info-text">
+                  I am sure I will be able to manage and make enough hours a week to meet your needs
+                  and expectations.
+                  <br />
+                  <br />
+                  I am ready to work with you and help your business grow.
+                  <br />
+                  <br />
+                  Regards
+                </p>
+                {resume && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resume}
+                    >
+                      Resume
+                    </a>
+                  </span>
+                )}
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/mock/data.js
@@ -0,0 +1,112 @@
+import uuidv1 from 'uuid/v1';
+
+// HEAD DATA
+export const headData = {
+  title: 'Yegor Portfolio', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: 'Yegor Yanushkevich',
+  subtitle: 'I am a senior Software Engineer.',
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'coverimage3.jpg',
+  paragraphOne:
+    'A seasoned full stack developer with a flair for creating elegant solutions in the least amount of time.',
+  paragraphTwo:
+    'Specialized in web application development such as E-Commerce, Enterprise Application, SaaS Platform, SPA, Health Care System, CRM and Learning Management System.',
+  paragraphThree: 'Love working with great team with Agile/Scrum methodology.',
+  paragraph4: ' - Front-End',
+  resume: 'https://drive.google.com/file/d/1olrccn_MDTrxXdvVjzdsebaRCvN5d-Ro/view?usp=sharing', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: '1',
+    img: 'upwork_gallery.jpg',
+    title: 'Upwok Experience',
+    info: 'I have 2 years of experience on upwork.',
+    info2: 'I completed 27 jobs, earned around $200k and worked 3999hrs on remote jobs.',
+    url: 'https://ibb.co/XVv4BW9',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'deskpro.png',
+    title: 'Helpdesk Software at Deskpro',
+    info:
+      'I was actively involved at development of Helpdesk Software Platform at Deskpro which founded 2002.',
+    info2: 'Used React.js, TypeScript, GraphQL, React-Storybook, React Hooks, Context.',
+    url: 'https://www.deskpro.com/',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'elliot.png',
+    title: 'Next generation E-Commerce system',
+    info: 'Involved at development of next generation e-commerce system at Elliot, Inc.',
+    info2: 'Used React.js, TypeScript, GraphQL and Formik, Context API, React Hooks, UI Kit.',
+    url: 'https://elliot.store/',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'lurn.png',
+    title: 'Entreprenure Online Platform',
+    info: 'Involved at development of Entreprenure Online Platform at Lurn.',
+    info2: 'Used PHP, Laravel 5.6, Vue.js.',
+    url: 'https://lurn.com/',
+    repo: '', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: 'yegoryanushkevich@gmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: uuidv1(),
+      name: 'angellist',
+      url: 'https://angel.co/u/yegor-yanushkevich',
+    },
+    {
+      id: uuidv1(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/yegor-yanushkevich-509460165/',
+    },
+    {
+      id: uuidv1(),
+      name: 'github',
+      url: 'https://github.com/yegor92',
+    },
+    {
+      id: uuidv1(),
+      name: 'laptop',
+      url: 'https://www.hiremotely.com/profile/Yegor-Y-full-stack-mobile-web-developer',
+    },
+    {
+      id: uuidv1(),
+      name: 'user',
+      url: 'https://www.guru.com/freelancers/yegor-yanushikevich',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
