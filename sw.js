7892683b993a84eccb3e3b6037eb2fb02c9a25bc
yegor <yegoryanushkevich@gmail.com>
2020-06-02 12:54:25 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d9f653739767379e25bd.js"
+    "url": "webpack-runtime-660c88c6bd059df092d5.js"
   },
   {
     "url": "framework-43898561dafbdf32aefd.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "edac481ba7b89be0b22c31b1beb5cd12"
+    "revision": "0d47c30876fad1b27250dd211159dda4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-2a242c05cade058abd28.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8f802d1b4482bf8eb4811c6114b601e9"
+    "revision": "627e34b86d953777fe409fb275af8d24"
   },
   {
     "url": "manifest.webmanifest",
